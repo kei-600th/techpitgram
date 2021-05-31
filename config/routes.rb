8ccc269ad6c243d22f576013182e97cf6d8b7a80
kei-600th <kei19941005@gmail.com>
2021-05-31 12:31:14 +0900

    仮のトップページを表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    # ここにpagesコントローラーのhomeアクションのルーティングを追加する
+    root 'pages#home'
 end

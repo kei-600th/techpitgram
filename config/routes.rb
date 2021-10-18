7bd8eb7ae1c5b9b91ba34d9ef24170a9717fd9d8
kei-600th <kei19941005@gmail.com>
2021-10-18 23:10:39 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    # ここにpagesコントローラーのhomeアクションのルーティングを追加する
+    root 'posts#index'
+    get '/users/:id', to: 'users#show', as: 'user'
+
+    resources :posts, only: %i(new create index show destroy) do
+      resources :photos, only: %i(create)
+      resources :likes, only: %i(create destroy)
+      resources :comments, only: %i(create destroy)
+    end
+    
+end
